7b6f2e7d6d7aced6a436c44bf7e8d044dc758b2a
Vivek <dev.vivekkhanal@gmail.com>
2023-08-17 02:53:05 +0530

    added youtube image

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -4,23 +4,39 @@ import React from "react";
 import galleryBanner from "../../assets/gallery/galleryBanner.webp";
 import { galleryPageData } from "@/helpers/mockData/galleryPageData";
 import Image from "next/image";
+import YouTube from 'react-youtube';
 
 const Gallery = () => {
+  const opts = {
+    height: '600',
+    width: '100%',
+ 
+  };
   return (
     <Layout>
       <div>
         <Banner pageName="Gallery" img={galleryBanner} />
         <div className="flex flex-wrap gap-5 my-5">
           {galleryPageData?.map((data, index) => {
-            return (
+            if (data?.type === "youtube")
+            {return (
               <div key={index} className="flex-auto  ">
-                <Image
-                  src={data?.img}
-                  alt={"img" + index}
-                  className=" h-full object-cover  w-full"
-                />
+             <YouTube
+  videoId={data?.link}  
+  opts={opts}                
+/>
               </div>
-            );
+            )} else {
+              return (
+                <div key={index} className="flex-auto  ">
+                  <Image
+                    src={data?.img}
+                    alt={"img" + index}
+                    className=" h-full object-cover  w-full"
+                  />
+                </div>
+              )
+            }
           })}
         </div>
       </div>
