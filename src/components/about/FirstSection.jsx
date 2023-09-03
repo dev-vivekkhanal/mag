6021f731a229fa103c4bba5258e0a3aefff5e773
Vivek <dev.vivekkhanal@gmail.com>
2023-09-03 12:28:33 +0530

    added grid in about page

--- src/components/about/FirstSection.jsx
+++ src/components/about/FirstSection.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import left_img from "../../assets/about/left_img.webp";
-import right_img from "../../assets/about/right_img.webp";
+import waves from "../../assets/home/bg-waves.png";
+import left_img from "../../assets/about/left_img.png";
+import right_img from "../../assets/about/right_img.png";
 import Image from "next/image";
 
 const FirstSection = () => {
   return (
-    <section className="  font-playfair text-center">
+    <section className="  font-playfair text-center reaative">
+      {/* <Image
+        src={waves}
+        fill
+        className="w-full opacity-30 hidden lg:block z-[-10]"
+      /> */}
       <div
         className={`flex flex-col lg:flex-row gap-5 lg:px-[2.5%] xl:px-[5%]  mx-auto justify-between bg-bottom py-32 lg:bg-[url('/_next/static/media/bg-lines.0efc976a.svg')] bg-no-repeat bg-cover `}
       >
@@ -14,26 +20,22 @@ const FirstSection = () => {
         </div>
 
         <div className="bg-white bg-opacity-60 white-bg-shadow px-[2.5%] lg:px-0 xl:w-[50%] mx-auto">
-          <h2 className=" text-lg md:text-2xl font-medium mb-10 text-accent_color">
-            Special Price
-          </h2>
           <h1 className="  text-xl md:text-3xl font-medium mb-10">
             Start Your Planning With Us
           </h1>
 
           <div className="font-lora md:text-xl space-y-8">
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa ad
-              a minus cumque ex minima velit explicabo nesciunt ea fuga soluta
-              at dolor similique quo error, veniam quidem dolores pariatur
-              suscipit
-            </p>
-
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa ad
-              a minus cumque ex minima velit explicabo nesciunt ea fuga soluta
-              at dolor similique quo error, veniam quidem dolores pariatur
-              suscipit
+              Dedicated to crafting everlasting impressions, by catering all
+              your weddings/Events needs as per your ritual, within your budget
+              and quality, to create exceptional projects. From intimate moments
+              to grand occasions, we refine them into lasting treasures. Our
+              creative ventures adhere to global excellence standards, etching
+              eternal memories with experience and passion. More than a company,
+              we're a family that seeks meaning in every shot, focusing on
+              elegance, class, and beauty. Framing your precious moments is our
+              passion, from outdoors to weddings, fashion to family. Our hands
+              create, our hearts dream. Let our work and passion speak volumes.
             </p>
           </div>
         </div>
--- src/components/about/SecondSection.jsx
+++ src/components/about/SecondSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import grid from "../../assets/about/grid.webp";
+import grid from "../../assets/about/grid.png";
 
 import Image from "next/image";
 
 const SecondSection = () => {
-  return <Image src={grid} alt="grid" className="mx-auto py-20" />;
+  return <Image src={grid} alt="grid" className="mx-auto py-20 w-[90%]" />;
 };
 
 export default SecondSection;
